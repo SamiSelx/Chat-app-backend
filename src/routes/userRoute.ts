49c4938f466028cf18091e2aab9c450cc09bbe45
SamiSelx <samifcb14@gmail.com>
2024-07-15 17:14:28 +0100

    add limiter timer

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -8,7 +8,7 @@ const router = express.Router()
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 10000, // limit each IP to 100 requests per windowMs
     message: "Too many requests, please try again later."
   });
 
